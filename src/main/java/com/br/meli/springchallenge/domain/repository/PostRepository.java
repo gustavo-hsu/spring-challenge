a33c99973a922dc9f6317485d7b75d4cae96298f
Gustavo <gustavo.wu@mercadolivre.com>
2021-06-01 20:45:56 -0300

    Fix query to include order by date and get only posts from two last weeks

--- src/main/java/com/br/meli/springchallenge/domain/repository/PostRepository.java
+++ src/main/java/com/br/meli/springchallenge/domain/repository/PostRepository.java
@@ -11,7 +11,10 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
 
     @Query(value = "select * from post p where " +
             "p.user_id in " +
-            "(select f.to_user_id from follower f where f.from_user_id = ?1)",
+            "(select f.to_user_id from follower f where f.from_user_id = ?1) " +
+            "AND p.date >= (CURRENT_DATE() - INTERVAL '14' DAY) "+
+            "AND p.date <= CURRENT_DATE() " +
+            "ORDER BY p.date DESC",
             nativeQuery = true)
     List<Post> getPostsByUserId(int userId);
 }
