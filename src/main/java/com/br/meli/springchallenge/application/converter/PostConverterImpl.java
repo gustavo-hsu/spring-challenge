a0c46dd7f1982d3b26dc17c60fa2ad38adc61454
Gustavo <gustavo.wu@mercadolivre.com>
2021-06-08 10:07:49 -0300

    Refactor - apply DRY principle
    Also extract validation to private method

--- src/main/java/com/br/meli/springchallenge/application/converter/PostConverterImpl.java
+++ src/main/java/com/br/meli/springchallenge/application/converter/PostConverterImpl.java
@@ -1,5 +1,6 @@
 package com.br.meli.springchallenge.application.converter;
 
+import com.br.meli.springchallenge.application.validator.UserValidator;
 import com.br.meli.springchallenge.domain.model.Category;
 import com.br.meli.springchallenge.domain.model.Post;
 import com.br.meli.springchallenge.domain.model.User;
@@ -7,7 +8,6 @@ import com.br.meli.springchallenge.domain.repository.CategoryRepository;
 import com.br.meli.springchallenge.domain.repository.UserRepository;
 import com.br.meli.springchallenge.dto.PostDTO;
 import com.br.meli.springchallenge.exceptions.ApiException;
-import com.br.meli.springchallenge.exceptions.BadRequestApiException;
 import com.br.meli.springchallenge.exceptions.NotFoundApiException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,28 +17,27 @@ import org.springframework.stereotype.Service;
 public class PostConverterImpl implements PostConverter {
     private UserRepository userRepository;
     private CategoryRepository categoryRepository;
+    private UserValidator userValidator;
 
     @Override
     public Post fromPostRequestToPostEntity(PostDTO request) throws ApiException {
         User user = userRepository.findById(request.getUserId()).orElse(null);
         Category category = categoryRepository.findById(request.getCategory()).orElse(null);
 
-        if (user == null) {
-            throw new NotFoundApiException("User with id " + request.getUserId() + " not found");
-        }
-
-        if (!user.isSeller()) {
-            throw new BadRequestApiException("User with id " + request.getUserId() + " is not a seller and can not publish posts");
-        }
-
-        if(category == null) {
-            throw new BadRequestApiException("Category with id " + request.getCategory() + " not found");
-        }
+        validateRequest(request, user, category);
 
         Post post = request.toPostEntity();
         post.getProduct().setCategory(category);
         post.setUser(user);
 
         return post;
+    }
+
+    private void validateRequest(PostDTO request, User user, Category category) throws ApiException {
+        userValidator.validateSeller(user);
+
+        if(category == null) {
+            throw new NotFoundApiException("Category with id " + request.getCategory() + " not found");
+        }
     }
 }
